e5d998ebd704c8e4ab224e7febf4ba360eb21c33
admin <admin@WAVY>
2021-05-31 21:11:06 -0500

    Add project files.

---
+++ FirstResponsiveWebAppSoulinthavong/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppSoulinthavong.Models;
+
+namespace FirstResponsiveWebAppSoulinthavong.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.AgeThisYear = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CalculateAge model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.AgeThisYear = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.AgeThisYear = "";
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppSoulinthavong/Models/CalculateAge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppSoulinthavong.Models
+{
+    public class CalculateAge
+    {
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1900,2020, ErrorMessage = "Year entered must be between 1900 and 2020.")]
+        public int? BirthYear { get; set; }
+
+        public string AgeThisYear()
+        {
+            const int THIS_YEAR = 2021;
+            int userAge;
+
+            userAge = THIS_YEAR - BirthYear.Value;
+
+            return this.Name + ", you will be " + userAge + " by the end of " + THIS_YEAR + ".";
+        }
+    }
+}
